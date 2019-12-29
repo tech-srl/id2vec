240a4a14fe9551ef469dbcaec8e85f1dbbc7bd3d
Izo Sakallah <izos@campus.technion.ac.il>
2019-12-29 23:10:15 +0200

    Delete utilities.ts

--- Common/utilities.ts
+++
@@ -1,9 +0,0 @@
-export function hashCode(str: string) : number{
-    var hash = 0;
-    for (let i = 0; i < str.length; i++) {
-        let chr = str.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-};
